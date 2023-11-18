a753e4c48caf1cd12b6fa31f6a1fe7aeb3a7657d
m-nadra <m.nadra@proton.me>
2023-11-18 18:55:31 +0100

    change print tasks from database

--- database.py
+++ database.py
@@ -5,6 +5,11 @@ def printTasks():
     con = sqlite3.connect('todo.db')
     cursor = con.cursor()
     cursor.execute("SELECT * FROM task;")
-    print(cursor.fetchall())
+    table = cursor.fetchall()
+    if len(table) == 0:
+        print("No tasks to do.")
+    else:
+        for i in table:
+            print(f"Task: {i[1]}, Deadline: {i[2]}")
     cursor.close()
     con.close()
