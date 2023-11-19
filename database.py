26b3dd6e35ca80685d17fe0f37af500193862030
m-nadra <m.nadra@proton.me>
2023-11-19 12:18:50 +0100

    add print today tasks option

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 
 def printTasks():
@@ -21,5 +22,21 @@ def addTask(name, deadline):
     userValues = (name, deadline)
     cursor.execute("INSERT INTO task(name, deadline) VALUES(?, ?)", userValues)
     con.commit()
+    cursor.close()
+    con.close()
+
+
+def printTodayTasks():
+    con = sqlite3.connect('todo.db')
+    cursor = con.cursor()
+    today = datetime.datetime.now()
+    queryDate = (f"{today.year}-{today.month}-{today.day}",)
+    cursor.execute("SELECT name FROM task WHERE deadline = ?;", queryDate)
+    table = cursor.fetchall()
+    if len(table) == 0:
+        print("No tasks to do.")
+    else:
+        for i in table:
+            print(f"Task: {i[0]}")
     cursor.close()
     con.close()
--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import database as db
 
 while True:
     print("""
-1) Today's tasks
-2) Add a task
+1) All tasks
+2) Today's tasks
+3) Add a task
 0) Exit
 """)
     userInput = int(input(">"))
@@ -11,6 +12,8 @@ while True:
         case 1:
             db.printTasks()
         case 2:
+            db.printTodayTasks()
+        case 3:
             taskName = input("Enter task name: ")
             taskDeadline = input("Enter date (yyyy-MM-dd): ")
             db.addTask(taskName, taskDeadline)
